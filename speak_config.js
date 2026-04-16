8eb9a831b1bf4e93557aa72366322c14977b329d
Jason Cho <choin337@gmail.com>
2026-04-16 11:00:23 +0900

    v0.24 생활중국어800 으로 변경하여 추가함

--- speak_config.js
+++ speak_config.js
@@ -26,11 +26,11 @@ const UID_MIGRATION_MAP = {
 
 const CONFIG = {
     savePageTitle: "Foreign Speaking",
-    studyFileName: 'studySpeakingData-v1.0.json',
+    studyFileName: 'studySpeakingData-v1.1.json',
     get studySaveName() {
         return getStudySaveName(this.studyFileName);
     },
-    version: "v0.22",
+    version: "v0.24",
     
     // UID 마이그레이션 맵 참조
     UID_MIGRATION_MAP: UID_MIGRATION_MAP,
